26420586a3417a627b21156c8e063d2631ce89e6
Gabriel Kuster <gabrielazevedo1812@gmail.com>
2025-07-10 21:07:44 -0300

    salvando em um arquivo, acho que tem que ser otimizado

--- srl/align_labels.py
+++ srl/align_labels.py
@@ -2,6 +2,8 @@
 from datasets import load_dataset
 from transformers import AutoTokenizer
 import preprocess 
+import os
+import json
 
 # %%
 model = 'neuralmind/bert-base-portuguese-cased' 
@@ -17,36 +19,40 @@ new_tokens = preprocess.preprocess_tokens(dataset['train'])
 def tokenize_and_align_labels (sentences,dataset_train):
     tokenizer = AutoTokenizer.from_pretrained(model)
     new_labels_aligned = dict ()
+    debug = 0
 
-    for index, sentences_to_tokenize in enumerate(sentences):
-        new_labels_aligned[index] = []
+    with open ("./out/toFineTunning.txt", "a") as f:
+        for index, sentences_to_tokenize in enumerate(sentences):
+            debug = debug + 1
+            print (debug)
+            new_labels_aligned[index] = []
 
-        tokenized_sentence = tokenizer(sentences_to_tokenize, 
-                                        is_split_into_words=True)
-        
-        words_ids = tokenized_sentence.word_ids()
+            tokenized_sentence = tokenizer(sentences_to_tokenize, 
+                                            is_split_into_words=True)
+            
+            words_ids = tokenized_sentence.word_ids()
 
-        frames_for_this_sentence = dataset_train['srl_frames'][index]
-        
-        for frame in frames_for_this_sentence:
-            prev_word_id = None
-            new_labels = []
-            labels = frame['frames']
-            verb = frame['verb']
+            frames_for_this_sentence = dataset_train['srl_frames'][index]
 
-            for word_id in words_ids:
-                if word_id is None:
-                    new_labels.append('O')
-                elif word_id != prev_word_id:
-                    new_labels.append(labels[word_id])  
-                else:
-                    new_labels.append('O')
-                prev_word_id = word_id
+            for frame in frames_for_this_sentence:
+                prev_word_id = None
+                new_labels = []
+                labels = frame['frames']
+                verb = frame['verb']
 
-            new_labels_aligned[index].append({'verb': verb, 'new_labels': new_labels}) 
+                for word_id in words_ids:
+                    if word_id is None:
+                        new_labels.append('O')
+                    elif word_id != prev_word_id:
+                        new_labels.append(labels[word_id])  
+                    else:
+                        new_labels.append('O')
+                    prev_word_id = word_id
 
-            print (new_labels_aligned)
-
-            # Aqui provavelmente para cada label eu vou ter que passar para ID
+            # Provavelmente aqui eu ja posso trocar de frames para os ids que o finetunner reconhece
+ 
+                json.dump(
+                        {'index': index,'verb': verb,'new_labels': new_labels}, f)
+                f.write('\n')
 
 tokenize_and_align_labels(new_tokens,dataset['train'])
