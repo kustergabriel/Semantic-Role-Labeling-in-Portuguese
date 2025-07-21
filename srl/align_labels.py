0575dad6c71729052aa68ae397d98934f1833919
Gabriel Kuster <gabrielazevedo1812@gmail.com>
2025-07-21 12:44:05 -0300

    consegui fazer o treino, porem deve ta tudo errado

--- srl/align_labels.py
+++ srl/align_labels.py
@@ -1,88 +1,51 @@
 # %%
 from datasets import load_dataset
-from transformers import AutoTokenizer
+from transformers import AutoTokenizer, AutoModel
 import preprocess 
-import os
-import json
-import labels2id as labels2id
 
 # %%
-model = 'neuralmind/bert-base-portuguese-cased' 
+model_name = "neuralmind/bert-base-portuguese-cased"
+model = AutoModel.from_pretrained(model_name)
 url = 'liaad/Propbank-BR'
-dataset = load_dataset(url, "default")
+dataset = load_dataset(url, 'flatten')
 fullDatasetTrain = dataset['train']
 fullDatasetTest = dataset['test']
+tokenizer = AutoTokenizer.from_pretrained(model_name)
 
 # %%
 # Tratando alguns caracteres que podem dar erro na tokenizacao
-newTokensTrain = preprocess.preprocess_tokens(fullDatasetTrain[0:100])
-newTokensTest = preprocess.preprocess_tokens(fullDatasetTest[0:30])
+newTokensTrain = preprocess.preprocess_tokens(fullDatasetTrain)
+newTokensTest = preprocess.preprocess_tokens(fullDatasetTest)
+
+updatedDatasetTrain = fullDatasetTrain.remove_columns('tokens')
+updatedDatasetTrain = updatedDatasetTrain.add_column('tokens', newTokensTrain)
+
+updatedDatasetTest = fullDatasetTest.remove_columns('tokens')
+updatedDatasetTest = updatedDatasetTest.add_column('tokens', newTokensTest)
 
 # %%
+def tokenize_and_align_labels (examples):
+    tokenized_inputs = tokenizer (examples['tokens'], truncation=True, is_split_into_words=True)
 
-def tokenize_and_align_labels (sentences,dataset):
-    tokenizer = AutoTokenizer.from_pretrained(model)
-    new_labels_aligned = dict ()
-    new_word_ids_dict = dict ()
-    new_word_ids_list = list ()
-
-
-    for index, sentences_to_tokenize in enumerate(sentences):
-        new_labels_aligned[index] = []
-
-        tokenized_sentence = tokenizer(sentences_to_tokenize, 
-        is_split_into_words=True, 
-        padding=True)
-
-        words_ids = tokenized_sentence.word_ids() # aqui ele eh rapido 
-
-        #print (f"words_ids : {words_ids}, index : {index}" )
-        #frames_for_this_sentence = dataset['srl_frames'][index] # aqui ele demora mto
-        #print(frames_for_this_sentence)
-        #print (f"words_ids: {words_ids}, frames in the sentence: {frames_for_this_sentence}")
-        
-        # Vou tentar percorrer o words_ids de outra forma
-        prev_word_id = None
-        for word_id in words_ids:
-            if word_id != prev_word_id and word_id is not None:
-                new_word_ids_list.append (word_id)
+    labels = []
+    for i, label in enumerate(examples[f'frames']):
+        word_ids = tokenized_inputs.word_ids(batch_index=i)
+        previous_word_idx = None
+        label_ids = []
+        for word_idx in word_ids:
+            if word_idx is None:
+                label_ids.append(-100)
+            elif word_idx != previous_word_idx: 
+                label_ids.append(label[word_idx])
             else:
-                pass
-            prev_word_id = word_id
+                label_ids.append(-100)
+            previous_word_idx = word_idx
+        labels.append(label_ids)
 
-            new_word_ids_dict = {'word_id' : new_word_ids_list}
-        print (words_ids)
-        print (new_word_ids_list)
+    tokenized_inputs["labels"] = labels
+    return tokenized_inputs
+    
+# %%
 
-        '''
-        words_ids = tokenized_sentence.word_ids()
-        
-        frames_for_this_sentence = dataset['srl_frames'][index]
-
-        for frame in frames_for_this_sentence:
-            prev_word_id = None
-            new_labels = []
-            labels = frame['frames']
-            verb = frame['verb']
-
-            for word_id in words_ids:
-                if word_id != prev_word_id and word_id is not None:
-                    new_labels.append(labels[word_id])
-                else:
-                    new_labels.append('O')
-                prev_word_id = word_id
-            
-            ids = [labels2id.label2id.get(label, 0) for label in new_labels]
-            
-            # Dicionario 
-            new_labels_aligned = {'index': index,
-                        'verb': verb,
-                        'new_labels': new_labels, 'ids' : ids,
-                        'input_ids' : tokenized_sentence['input_ids'],
-                        'attention_mask' : tokenized_sentence ['attention_mask']
-                        }
-
-            print (new_labels_aligned)
-
-'''
-tokenize_and_align_labels(newTokensTrain,fullDatasetTrain)
+tokenizedDataset = dataset.map(tokenize_and_align_labels, batched=True)
+print (tokenizedDataset['train']['frames'])
---
+++ srl/fineTuning.py
@@ -0,0 +1,61 @@
+# %%
+from transformers import AutoModelForTokenClassification, TrainingArguments, Trainer, DataCollatorForTokenClassification
+from huggingface_hub import login
+import align_labels
+from seqeval.metrics import classification_report, accuracy_score, f1_score, precision_score, recall_score
+
+
+
+modelFineTuning = AutoModelForTokenClassification.from_pretrained('neuralmind/bert-base-portuguese-cased', num_labels=61)
+data_collator = DataCollatorForTokenClassification(align_labels.tokenizer)
+login("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")
+
+small_train_dataset = align_labels.tokenizedDataset["train"].shuffle(seed=42).select(range(1000))
+small_eval_dataset = align_labels.tokenizedDataset["test"]
+
+
+def compute_metrics(p):
+    predictions, labels = p
+    predictions = predictions.argmax(-1)
+
+    true_predictions = [
+        [align_labels.label_list[pred] for pred, label in zip(prediction, label) if label != -100]
+        for prediction, label in zip(predictions, labels)
+    ]
+    true_labels = [
+        [align_labels.label_list[label] for pred, label in zip(prediction, label) if label != -100]
+        for prediction, label in zip(predictions, labels)
+    ]
+
+    return {
+        "precision": precision_score(true_labels, true_predictions),
+        "recall": recall_score(true_labels, true_predictions),
+        "f1": f1_score(true_labels, true_predictions),
+        "accuracy": accuracy_score(true_labels, true_predictions),
+    }
+
+# %%
+# Nao configurei, apenas peguei do hugging face
+training_args = TrainingArguments(
+    output_dir="./results",
+    eval_strategy="epoch",
+    learning_rate=2e-5,
+    per_device_train_batch_size=16,
+    per_device_eval_batch_size=16,
+    num_train_epochs=3,
+    weight_decay=0.01,
+    push_to_hub=False,
+)
+
+trainer = Trainer(
+    model=modelFineTuning,
+    args=training_args,
+    train_dataset=small_train_dataset,
+    eval_dataset=small_eval_dataset,
+    processing_class=align_labels.tokenizer,
+    data_collator=data_collator,
+    compute_metrics=compute_metrics
+)
+
+# %%
+trainer.train()
